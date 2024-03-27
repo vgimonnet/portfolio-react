11bd9f5ee4c74c4406d52ea30c1484f5ca96b716
vgimonnet <valentin.gimonnet18@gmail.com>
2024-03-27 10:46:45 +0100

    test animation home section

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -36,9 +36,10 @@ const Home = () => {
 
     if (index >= INTRO.length) {
       clearTimeout(timeout);
-      setShowDetail(true);
-      if (titleContainer) {
-        titleContainer.classList.add('goup')
+      // setShowDetail(true);
+      const nav = document.getElementById('nav');
+      if (nav) {
+        nav.classList.add('test');
       }
     }
     
@@ -46,19 +47,24 @@ const Home = () => {
   }, [index])
 
   return (
-    <section id="home" className="h-full relative">
-      <h1 id="title-container" className="text-center text-7xl absolute top-2/4 translate-y-1/4"></h1>
-      <Transition.Root className="flex flex-row justify-center mt-60 gap-24 mx-auto" show={showDetail}>
-        <FadeIn delay="delay-[0ms]">
+    <section id="home" className="h-full relative flex justify-around items-center flex-col">
+      <h1 id="title-container" className="text-center text-7xl"></h1>
+      <nav id="nav" className="flex flex-row justify-center gap-24 mx-auto transition opacity-0">
+        <button onClick={() => setActiveSection('about')} className="bounce px-4 py-2 border border-white rounded w-36">About</button>
+        <button onClick={() => setActiveSection('projects')}  className="bounce px-4 py-2 border border-white rounded w-36">Projects</button>
+        <button onClick={() => setActiveSection('contact')} className="bounce px-4 py-2 border border-white rounded w-36">Contact</button>
+      </nav>
+      {/* <Transition.Root className="flex flex-row justify-center gap-24 mx-auto" show={showDetail}>
+        <FadeIn delay="delay-[100ms]">
           <button onClick={() => setActiveSection('about')} className="bounce px-4 py-2 border border-white rounded w-36">About</button>
         </FadeIn>
-        <FadeIn delay="delay-[300ms]">
+        <FadeIn delay="delay-[400ms]">
           <button onClick={() => setActiveSection('projects')}  className="bounce px-4 py-2 border border-white rounded w-36">Projects</button>
         </FadeIn>
-        <FadeIn delay="delay-[500ms]">
+        <FadeIn delay="delay-[600ms]">
           <button onClick={() => setActiveSection('contact')} className="bounce px-4 py-2 border border-white rounded w-36">Contact</button>
         </FadeIn>
-      </Transition.Root>
+      </Transition.Root> */}
     </section>
   )
 };
